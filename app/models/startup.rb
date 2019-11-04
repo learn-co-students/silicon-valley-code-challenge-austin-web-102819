831d094bdf2a984a92c6b12788dc697ae3a40dda
github username <github email address>
2019-11-04 09:50:46 -0600

    Submitting lab

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,58 @@
 class Startup
-
+    attr_accessor :name
+    attr_reader :founder, :domain
+    @@all = []
+    @@domains = []
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+        @@domains << domain
+    end
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+    def self.all
+        @@all
+    end
+    def self.find_by_founder(founder)
+        @@all.find { |k| k.founder == founder}
+    end
+    def self.domains
+        @@domains
+    end
+    def sign_contract(capitalist, type, investment)
+        FundingRound.new(self, capitalist, type, investment)
+    end
+    def num_funding_rounds
+        FundingRound.all.select{ |fund| fund.startup.name == self.name}.count
+    end
+    def total_funds
+        total = 0
+        arr = FundingRound.all.select{ |fund| fund.startup.name == self.name}
+        arr.each do |fun|
+                total += fun.investment
+        end
+        total
+    end
+    def investors
+        total = []
+        arr = FundingRound.all.select{ |fund| fund.startup.name == self.name}
+        arr.each do |fun|
+                total << fun.capitalist.name
+        end
+        total
+    end
+    def big_investors
+        total = []
+        arr = FundingRound.all.select{ |fund| fund.startup.name == self.name}
+        arr.each do |fun|
+            if VentureCapitalist.tres_commas_club.find {|cap| cap == fun.capitalist}
+            total << fun.capitalist.name
+            end
+        end
+        total
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,12 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
-
+microsoft = Startup.new("Microsoft", "Bill Gates", "technology")
+apple = Startup.new("Apple", "Steve Jobs", "technology")
+steve = VentureCapitalist.new("Steve Jobs", 100000000000)
+bill = VentureCapitalist.new("Bill Gates", 100000)
+microsoft.sign_contract(bill, "Angel", 100000)
+apple.sign_contract(steve, "Angel", 100000)
+microsoft.sign_contract(steve, "Angel", 10000)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
